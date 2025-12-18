f8f6f8b9990cffbd4eccba36ce6ee86b1d5af787
Chandan Kumar <chandan@izooto.com>
2025-12-18 11:53:57 +0530

    addpom

--- src/main/java/Pageobject/Registationpage.java
+++ src/main/java/Pageobject/Registationpage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
-import org.testng.Assert;
+
 
 
 public class Registationpage {
